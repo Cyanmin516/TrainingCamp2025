d289913e66b27164fe40b31207ce31f8148255fc
Cyanmin <cyanmin516@gmail.com>
2025-08-20 12:28:17 +0800

    variable name changed

--- 硬乙第一站程式.vb
+++ 硬乙第一站程式.vb
@@ -1,15 +1,16 @@
 Public Class Form1
-    Dim ledColor, greenQuery(16), redQuery(9), greenIndex, redIndex As Integer
+    Dim color, greenQ(16), redQ(9), gi, ri As Integer
+ 
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        ledColor = 1
-        redIndex = 0
+        color = 1
+        ri = 0
 
     End Sub
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        ledColor = 2
-        greenIndex = 0
+        color = 2
+        gi = 0
     End Sub
 
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
@@ -31,27 +32,27 @@ Public Class Form1
     End Sub
 
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
-        Dim s = ShapeContainer1.Shapes
-        greenQuery = {&H1, &H3, &H6, &HC, &H18, &H30, &H60, &HC0, &H60, &H30, &H18, &HC, &H6, &H3, &H1, 0}
-        redQuery = {&HC0, &H60, &H30, &H18, &HC, &H6, &H3, &H1, 0}
         Label1.Text = "Current Time: " + TimeString
+        Dim s = ShapeContainer1.Shapes
+        greenQ = {&H1, &H3, &H6, &HC, &H18, &H30, &H60, &HC0, &H60, &H30, &H18, &HC, &H6, &H3, &H1, 0}
+        redQ = {&HC0, &H60, &H30, &H18, &HC, &H6, &H3, &H1, 0}
         If SerialPort1.IsOpen Then
+            LedOff()
             For i = 0 To 7
                 s(i).fillcolor = Color.DarkRed
                 s(i + 8).fillcolor = Color.DarkGreen
             Next
-            LedOff()
             For i = 0 To 10 ^ 8
             Next
-            If ledColor = 1 And redQuery(redIndex) > 0 Then
-                SerialPort1.Write("R" & redQuery(redIndex))
-                Display(ledColor, redQuery(redIndex))
-                redIndex += 1
+            If color = 1 And redQ(ri) > 0 Then
+                SerialPort1.Write("R" & redQ(ri))
+                Display(redQ(ri))
+                ri += 1
             End If
-            If ledColor = 2 And greenQuery(greenIndex) > 0 Then
-                SerialPort1.Write("G" & greenQuery(greenIndex))
-                Display(ledColor, greenQuery(greenIndex))
-                greenIndex += 1
+            If color = 2 And greenQ(gi) > 0 Then
+                SerialPort1.Write("G" & greenQ(gi))
+                Display(greenQ(gi))
+                gi += 1
             End If
         Else
             For i = 0 To 15
@@ -63,11 +64,11 @@ Public Class Form1
         SerialPort1.Write("R0")
         SerialPort1.Write("G0")
     End Sub
-    Private Sub Display(ByVal ledColor, ByVal no)
+    Private Sub Display(ByVal no)
         Dim s = ShapeContainer1.Shapes
         For i = 0 To 7
-            If no Mod 2 = 1 And ledColor = 1 Then s(i).fillcolor = Color.Red
-            If no Mod 2 = 1 And ledColor = 2 Then s(i + 8).fillcolor = Color.GreenYellow
+            If no Mod 2 = 1 And color = 1 Then s(i).fillcolor = Color.Red
+            If no Mod 2 = 1 And color = 2 Then s(i + 8).fillcolor = Color.GreenYellow
             no \= 2
         Next
     End Sub
